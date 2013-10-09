9ed4a9e35b2b89d585551d9554c9a5cce7f03220
James Neely <jgn223@icloud.com>
2013-10-09 19:54:54 -0400

    Initial Commit

---
+++ Oct10/Oct10AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Oct10AppDelegate.h
+//  Oct10
+//
+//  Created by James Neely on 10/9/13.
+//  Copyright (c) 2013 James Neely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Oct10AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Oct10/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Oct10
+//
+//  Created by James Neely on 10/9/13.
+//  Copyright (c) 2013 James Neely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Oct10AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Oct10AppDelegate class]));
+    }
+}
